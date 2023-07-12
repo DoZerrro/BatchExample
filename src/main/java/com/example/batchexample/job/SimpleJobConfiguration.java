ccaaa37d2223b148fc940f711e72539d300b1cc8
DoZerrro <ehdud1515@naver.com>
2023-07-12 15:49:58 +0900

    20230712 Batch 부가 설명 작성

--- src/main/java/com/example/batchexample/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/batchexample/job/SimpleJobConfiguration.java
@@ -12,24 +12,28 @@ import org.springframework.context.annotation.Configuration;
 
 @Slf4j  // log 사용을 위한 lombok 어노테이션
 @RequiredArgsConstructor    // Constructor DI를 위한 lombok 어노테이션
-@Configuration
+@Configuration  // Spring Batch 의 모든 Job 은 @Configuration 으로 등록해서 사용한다.
 public class SimpleJobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+    /**
+     * Batch Job 을 생성하는 simpleJob 은 simpleStep1 을 품고 있다.
+     */
+
     @Bean
     public Job simpleJob() {
-        return jobBuilderFactory.get("simpleJob")
+        return jobBuilderFactory.get("simpleJob")   // simpleJob 이란 이름의 Batch Job을 생성한다. job 의 이름을 별도로 지정하지 않고 Builder 를 통해 지정한다.
                 .start(simpleStep1())
                 .build();
     }
 
     @Bean
     public Step simpleStep1() {
-        return stepBuilderFactory.get("simpleStep1")
-                .tasklet((contribution, chunkContext) -> {
-                    log.info(">>>>>>>>>> This is Step1");
+        return stepBuilderFactory.get("simpleStep1")    // simpleStep1 이란 이름의 Batch Step 을 생선한다. jobBuilderFactory 와 동일하게 Builder를 통해 이름을 지정한다.
+                .tasklet((contribution, chunkContext) -> {  // Step 안에서 수행될 기능들을 명시한다. Tasklet Step 안에서 단일로 수행될 커스텀한 기능들을 선언할 때 사용한다.
+                    log.info(">>>>>>>>>> This is Step1");   // Batch 가 수행되면 log.info 가 출력한다.
                     return RepeatStatus.FINISHED;
                 })
                 .build();
